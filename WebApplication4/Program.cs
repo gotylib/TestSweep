c8399bb0fc422127cb96de9308c5299d2f670c7c
Gleb Mihalev <mixalev702@mail.ru>
2025-01-20 17:49:59 +0300

    v2

--- WebApplication4/Program.cs
+++ WebApplication4/Program.cs
@@ -1,5 +1,17 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
